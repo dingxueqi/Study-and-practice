73eea36f894772a393e33281692c00ded53237f3
dingxueqi <dingxueqi829@163.com>
2018-09-18 16:55:46 +0800

    蛇形矩阵

--- c_study/char_practice/18_char.c
+++ c_study/char_practice/18_char.c
@@ -34,40 +34,43 @@
 int main(void)
 {
     int i=0,j=0,k=0,input_num = 0,sum=0,temp=0;;
-    int *output=NULL;
-    scanf("%d",&input_num);
-
-    for(i=1;i<=input_num;i++)
-    {
-        sum += i;/*计算要存储多少个数据*/
+    int *output = NULL;
+    while(scanf("%d",&input_num)!=EOF){
+        for(i=1;i<=input_num;i++)
+        {
+            sum += i;/*计算要存储多少个数据*/
+        }
+        output = (int *)malloc(sizeof(int) * sum);/*申请需要存储数据的空间大小*/
+        memset(output,0,sizeof(int)*sum);
+        for(i=1;i<=input_num;i++)
+        {
+            output[i-1]= temp +i;
+            temp = output[i-1];/*计算并存储第一排中的数据*/
+        }
+        k=input_num;
+        temp = input_num;
+        for(i=1;i<=input_num;i++)
+        {
+            for(j=i;j<input_num;j++)
+            {
+                output[k] = output[k-temp+1]-1;
+                k++;
+            }
+            temp--;
+        }
+        for(i=0,k=0;i < input_num;i++)
+        {
+            for(j=i;j < input_num;j++)
+            {
+                if(j==(input_num-1))
+                   printf("%d",output[k]);
+                else
+                    printf("%d ",output[k]);
+                k++;
+            }
+            printf("\n");
+        }
+        free(output);
     }
-	output = (int *)malloc(sizeof(int) * sum);/*申请需要存储数据的空间大小*/
-	memset(output,0,sizeof(int)*sum);
-	for(i=1;i<=input_num;i++)
-	{
-		output[i-1]= temp +i;
-		temp = output[i-1];/*计算并存储第一排中的数据*/
-	}
-	k=input_num;
-	temp = input_num;
-	for(i=1;i<=input_num;i++)
-	{
-		for(j=i;j<input_num;j++)
-		{
-			output[k] = output[k-temp+1]-1;
-			k++;
-		}
-		temp--;
-	}
-	for(i=0,k=0;i<input_num;i++)
-	{
-		for(j=i;j<input_num;j++)
-		{
-			printf("%d ",output[k]);
-			k++;
-		}
-		printf("\n");
-	}
-    free(output);
     return 0;
 }
