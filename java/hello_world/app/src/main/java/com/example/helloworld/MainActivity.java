fc42f11d6a0ae1b3e2fcee675411710f50d05a1c
dingxueqi <dingxueqi829@163.com>
2020-01-03 15:41:41 +0800

    修改内容，格式，字体等

--- java/hello_world/app/src/main/java/com/example/helloworld/MainActivity.java
+++ java/hello_world/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //获取名字为tv_hello的TextView控件
+        TextView show_hello= (TextView) findViewById(R.id.tv_hello);//其中R.id.tv_hello定义在src/main/res/layout/activity_main.xml
+        //给TextView控件设置文字内容
+        show_hello.setText("今天天气真冷啊，哈哈哈哈");
+        //给TextView控件设置文字颜色
+        show_hello.setTextColor(Color.RED);
+        //给TextView控件设置文字大小
+        show_hello.setTextSize(30);
     }
 }
